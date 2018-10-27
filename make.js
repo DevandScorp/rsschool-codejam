0477156c1188be74998410008c8fcae3824290af
DevandScorp <artemda459@gmail.com>
2018-10-28 01:32:41 +0300

    feat: add tests for sumOfOther and make functions

--- make.js
+++ make.js
@@ -1,7 +1,12 @@
-function make(...args) {
+module.exports.make = function make(...args) {
   if (typeof args[args.length - 1] === 'function') {
     const func = args.pop();
     return args.reduce((acc, next) => func(acc, next));
   }
   return make.bind(null, ...args);
-}
+};
+module.exports.multiply = (a, b) => a * b;
+module.exports.add = (a, b) => a + b;
+module.exports.divide = (a, b) => a / b;
+module.exports.substract = (a, b) => a - b;
+module.exports.nod = (a, b) => a % b;
--- sumOfOther.js
+++ sumOfOther.js
@@ -1,8 +1,9 @@
-function sumOfOther(arr) {
+module.exports.sumOfOther = function sumOfOther(arr) {
   if (!arr) return [];
+  if (arr.length === 1) return arr;
   const res = [];
   for (let i = 0; i < arr.length; i += 1) {
     res[i] = (arr.filter((elem, index) => index !== (+i)).reduce((acc, curVal) => acc + curVal));
   }
   return res;
-}
+};
